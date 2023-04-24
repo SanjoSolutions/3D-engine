2f5daab2b2852a7acb3812ed4ec72e16cd62d2ca
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-24 20:09:59 +0200

    Fill

--- src/index.js
+++ src/index.js
@@ -4,22 +4,50 @@ const Y_AXIS_ANGLE = convertDegreeToRadian(-45)
 
 function render(context, cube) {
   context.beginPath()
-  context.rect(cube.x, cube.z, cube.width, cube.height)
 
   context.moveTo(cube.x, cube.z)
-  context.lineTo(cube.x + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
+  context.lineTo(cube.x + cube.width, cube.z)
+  context.lineTo(cube.x + cube.width, cube.z + cube.height)
+  context.lineTo(cube.x, cube.z + cube.height)
+  context.closePath()
+
+
+  context.moveTo(cube.x, cube.z)
+  context.lineTo(
+    cube.x + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE),
+    cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE),
+  )
+  context.lineTo(cube.x +
+    cube.width +
+    0.5 *
+    cube.depth *
+    Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
+  context.lineTo(cube.x + cube.width, cube.z)
+  context.closePath()
+
 
   context.moveTo(cube.x + cube.width, cube.z)
-  context.lineTo(cube.x + cube.width + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
+  context.lineTo(cube.x +
+    cube.width +
+    0.5 *
+    cube.depth *
+    Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
+  context.lineTo(
+    cube.x +
+    cube.width +
+    0.5 *
+    cube.depth *
+    Math.cos(Y_AXIS_ANGLE),
+    cube.z + cube.height + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE),
+  )
+  context.lineTo(cube.x + cube.width, cube.z + cube.height)
+  context.closePath()
 
-  context.moveTo(cube.x + cube.width, cube.z + cube.height)
-  context.lineTo(cube.x + cube.width + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + cube.height + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
 
-  context.moveTo(cube.x + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
-  context.lineTo(cube.x + cube.width + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
-
-  context.moveTo(cube.x + cube.width + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
-  context.lineTo(cube.x + cube.width + 0.5 * cube.depth * Math.cos(Y_AXIS_ANGLE), cube.z + cube.height + 0.5 * cube.depth * Math.sin(Y_AXIS_ANGLE))
+  if (cube.color) {
+    context.fillStyle = cube.color
+    context.fill()
+  }
 
   context.stroke()
 }
@@ -41,3 +69,15 @@ const cube = {
 }
 
 render(context, cube)
+
+const cube2 = {
+  width: 50,
+  depth: 50,
+  height: 50,
+  x: 150,
+  y: 0,
+  z: 50,
+  color: "yellow",
+}
+
+render(context, cube2)
